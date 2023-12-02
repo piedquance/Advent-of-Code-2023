a2ba2ee8db5a5398c33476c558aa6301dd0d242b
stackfold <shutterstacks@gmail.com>
2023-12-02 13:06:02 -0500

    removed day2 code from Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	try {
-        File myObj = new File("input2.txt");
-        Scanner myReader = new Scanner(myObj);
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-
-
-
-            //System.out.println(data);
-        }
-        myReader.close();
-    } catch (FileNotFoundException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-    }
+	
     }
 }
 
