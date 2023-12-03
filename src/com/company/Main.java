452c141e4004a332a5bb0444302612015e9cc582
stackfold <shutterstacks@gmail.com>
2023-12-03 08:45:45 -0500

    day3 advancements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,11 @@ public class Main {
     public static void main(String[] args) {
         //day2.day2();
         day3.day3();
+        //char[] a = {'a', 'b', 'c'};
+        //String b = "abc";
+        //System.out.println(new String(a).contains(b.charAt(0) + ""));
+
+
     }
 }
 
--- src/com/company/day3.java
+++ src/com/company/day3.java
@@ -8,40 +8,51 @@ public class day3 {
     public static void day3() {
         int y = 0;
         ArrayList<HashMap<Integer, Boolean>> grid = new ArrayList<>(140);
+        for(HashMap<Integer, Boolean> hm : grid) {
+
+        }
         LinkedList<Coordinate> coordinates = new LinkedList<>();
-        String[] symbols = {"+", "#", "@", "$", "-", "*", "/", "=", "&"};
-        String[] numbers = {"1","2","3","4","5","6","7","8","9","0"};
+        char[] symbols = {'+', '#', '@', '$', '-', '*', '/', '=', '&'};
+        String numbers = "123456789";
 
         try {
-            File myObj = new File("input2.txt");
+            File myObj = new File("input3.test.txt");
             Scanner myReader = new Scanner(myObj);
 
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                y++;
-                for(int x = 0; x < data.length(); x++) {
-                    if(Arrays.asList(symbols).contains(data.charAt(x))) {
+                System.out.print("Line: " + y + " ");
 
+                for(int x = 0; x < data.length(); x++) {
+                    if(new String(symbols).contains(data.charAt(x) + "")) {
+                        System.out.print(data.charAt(x));
+                        grid.get(y).put(x, true);
                     }
-                    else if(Arrays.asList(numbers).contains(data.charAt(x))) {
+                    else if(new String(numbers).contains(data.charAt(x) + "")) {
                         String num = data.charAt(x) + "";
                         int offset = 1;
-                        while(Arrays.asList(numbers).contains(data.charAt(x+offset))) {
-                            offset++;
+                        while(new String(numbers).contains(data.charAt(x + offset) + "")) {
                             num += data.charAt(x+offset);
+                            offset++;
                         }
                         int Inum = Integer.parseInt(num);
                         coordinates.add(new Coordinate(Inum, x, x+offset));
                         x += offset;
                     }
                 }
-
+                y++;
+                System.out.println("");
             }
             myReader.close();
         } catch(FileNotFoundException e) {
             System.out.println("An error has occured");
             e.printStackTrace();
         }
+
+        for(Coordinate c : coordinates) {
+            System.out.println(c);
+        }
+        System.out.println(grid.get(0).get(0));
     }
 
     public class Row {
@@ -59,6 +70,11 @@ public class day3 {
         public int value;
         public int start;
         public int end;
+
+        @Override
+        public String toString() {
+            return value + " " + start + " " + end;
+        }
     }
 
 
